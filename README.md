765af200930acbcb7501a6b2c3ef55e4ae0d87f6
Jamie Hall <the.sly.marbo@googlemail.com>
2014-06-03 06:56:54 +0100

    Improved documentation

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-spin
-====
+# spin
 
-spin provides a simple spinlock for Go
+spin provides a simple spinlock for Go. Spinlocks should be used in
+cases where a mutex would be too much overhead and the lock will not
+be held for long.
+
+The most common use is for protecting a single variable that is
+changed briefly.
+
+```go
+type Counter struct {
+	c int
+	l spin.Lock
+}
+
+func (c *Counter) Inc() {
+	c.l.Lock()
+	c.c++
+	c.l.Unlock()
+}
+```
--- lock.go
+++ lock.go
@@ -1,3 +1,21 @@
+// Package spin provides a simple spinlock. Spinlocks should be used in
+// cases where a mutex would be too much overhead and the lock will not
+// be held for long.
+//
+// The most common use is for protecting a single variable that is
+// changed briefly.
+//
+//		type Counter struct {
+//			c int
+//			l spin.Lock
+//		}
+//
+//		func (c *Counter) Inc() {
+//			c.l.Lock()
+//			c.c++
+//			c.l.Unlock()
+//		}
+//
 package spin
 
 import (
